3c5bd65fc19a5ccb60eba7d1f2f9e985860dcf54
Ayca Dogan <aycadgan@hotmail.com>
2019-02-24 16:21:56 +0100

    Link component

--- src/main/resources/public/components/Article.js
+++ src/main/resources/public/components/Article.js
@@ -2,6 +2,7 @@ const Article = (props) => (
     <div>
         <Title text={props.title}></Title>
         <p>{props.text}</p>
+        <Link text="Facebook" url="https://www.facebook.com/" />
     </div>
 
 );
